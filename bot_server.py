e1116478ad32c5ec8474e063a34ffc963deaa547
Volmont85 <volmont85@gmail.com>
2026-03-25 12:10:27 +0300

    Update bot_server.py

--- bot_server.py
+++ bot_server.py
@@ -179,9 +179,9 @@ async def handle_inn(msg: Message, state: FSMContext):
             )
             await state.update_data(inn=inn, company_name=company)
             async with SessionLocal() as session:
-            await session.execute(text("""
-            UPDATE inbox SET inn=:inn, company_name=:nm WHERE telegram_id=:tg AND zakupka_num=:znum
-            """), {"inn": inn, "nm": name, "tg": msg.from_user.id, "znum": data["zakupka"]})
+                await session.execute(text("""
+                UPDATE inbox SET inn=:inn, company_name=:nm WHERE telegram_id=:tg AND zakupka_num=:znum
+                """), {"inn": inn, "nm": name, "tg": msg.from_user.id, "znum": data["zakupka"]})
             await session.commit()
             await msg.answer("✅ Заявка сохранена и передана на обработку в 1С.")
             await state.clear()
