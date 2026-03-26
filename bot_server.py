6a1210c89b810df080b0af55102ec9b78d41620f
Volmont85 <volmont85@gmail.com>
2026-03-26 09:37:22 +0300

    Update bot_server.py

--- bot_server.py
+++ bot_server.py
@@ -588,7 +588,7 @@ if __name__ == "__main__":
         uvicorn.run(
             "bot_server:app",
             host="0.0.0.0",
-            port=int(os.getenv("PORT", 8000)),
+            port=int(os.getenv("PORT", $PORT)),
         )
     else:
         # Локальный режим
