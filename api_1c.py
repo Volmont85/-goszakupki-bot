0064205c4d4ae04de8bad43d2dc55f59d09200ab
Volmont85 <volmont85@gmail.com>
2026-03-29 13:35:52 +0300

    Update api_1c.py

--- api_1c.py
+++ api_1c.py
@@ -35,7 +35,7 @@ async def api_inbox(api_key: str = Header(None)):
                 SELECT id, telegram_id, inn, company_name,
                        zakupka_num, message, zakupka_number
                 FROM inbox
-                WHERE status = 'new'
+                WHERE status = 'new' and (inn IS NULL OR TRIM(inn) = '')
             """))
             data = [dict(r._mapping) for r in res.fetchall()]
         return data
